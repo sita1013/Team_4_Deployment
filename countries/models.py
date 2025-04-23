f858d04912d4ca0f39048d5ed80d78e2061078ca
sita1013 <seluvaiasariahita@gmail.com>
2025-04-23 15:29:56 +0100

    added income levels to barchart and pm25lookup htmls

--- countries/models.py
+++ countries/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Country(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=3, unique=True)
-
     def __str__(self):
         return self.name
 
@@ -11,10 +10,15 @@ class PM25Record(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='pm25_records')
     year = models.IntegerField()
     value = models.FloatField(null=True, blank=True)
-
     class Meta:
         unique_together = ('country', 'year')
         ordering = ['country', 'year']
-
     def __str__(self):
         return f"{self.country.name} - {self.year}: {self.value}"
+    
+class CountryMetadata(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    income_level = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.code} - {self.income_level}"
+
--- countries/views.py
+++ countries/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from .models import PM25Record, Country
 from django.db.models import Max
+from .models import CountryMetadata
+from countries.models import Country, PM25Record, CountryMetadata
 
 def homepage(request):
-    
     latest_year = PM25Record.objects.aggregate(Max('year'))['year__max']
-
     top_records = PM25Record.objects.filter(year=latest_year).order_by('-value')[:5]
-
     countries = [record.country.name for record in top_records]
     values = [record.value for record in top_records]
-
     return render(request, 'countries/homepage.html', {
         'countries_json': countries,
         'values_json': values,
@@ -23,18 +21,19 @@ def pm25_lookup_view(request):
     countries = Country.objects.order_by('name')
     years = PM25Record.objects.values_list('year', flat=True).distinct().order_by('year')
     record = None
-    yearly_records = []
+    country = None
+    income_level = None
     year_list = []
     value_list = []
-    country_name = ""
     if selected_country:
+        country = Country.objects.filter(code=selected_country).first()
+        metadata = CountryMetadata.objects.filter(code=selected_country).first()
+        income_level = metadata.income_level if metadata else "Unknown"
         yearly_records = PM25Record.objects.filter(
             country__code=selected_country
         ).order_by('year')
         year_list = [r.year for r in yearly_records]
         value_list = [r.value for r in yearly_records]
-        if yearly_records:
-            country_name = yearly_records[0].country.name
     if selected_country and selected_year:
         record = PM25Record.objects.filter(
             country__code=selected_country,
@@ -48,7 +47,8 @@ def pm25_lookup_view(request):
         'record': record,
         'year_list': year_list,
         'value_list': value_list,
-        'country_name': country_name,
+        'country': country,
+        'income_level': income_level,
     }
     return render(request, 'countries/pm25_lookup.html', context)
 
@@ -59,9 +59,18 @@ def barchart_compare(request):
     country1_code = request.GET.get('country1')
     country2_code = request.GET.get('country2')
     record1 = record2 = None
+    income1 = income2 = None
     if selected_year and country1_code and country2_code:
         record1 = PM25Record.objects.filter(country__code=country1_code, year=selected_year).first()
         record2 = PM25Record.objects.filter(country__code=country2_code, year=selected_year).first()
+        try:
+            income1 = CountryMetadata.objects.get(code=country1_code).income_level
+        except CountryMetadata.DoesNotExist:
+            income1 = "Unknown"
+        try:
+            income2 = CountryMetadata.objects.get(code=country2_code).income_level
+        except CountryMetadata.DoesNotExist:
+            income2 = "Unknown"
     return render(request, 'countries/barchart_compare.html', {
         'countries': countries,
         'years': years,
@@ -70,4 +79,8 @@ def barchart_compare(request):
         'country2_code': country2_code,
         'record1': record1,
         'record2': record2,
+        'income1': income1, 
+        'income2': income2, 
     })
+
+
