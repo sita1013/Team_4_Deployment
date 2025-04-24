e30c10e5f11e15d271676d44fb824d1109d03108
sita1013 <seluvaiasariahita@gmail.com>
2025-04-24 12:21:52 +0100

    secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s)zb-zgttnbnv0daq=-9f&jl6@i5xa5d4&ubo73#@mgc*35u!%'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
