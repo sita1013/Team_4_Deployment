f91806c3d02c9eaa04b7ceed3bd9d4ec3e578026
sita1013 <seluvaiasariahita@gmail.com>
2025-04-24 18:56:34 +0100

    Add metadata routes directly in countries.urls for debugging

--- countries/urls.py
+++ countries/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('pm25/', views.pm25_lookup_view, name='pm25_lookup'),
     path('pm25/compare/', views.barchart_compare, name='barchart_compare'),
+
+    path('load-metadata/', views.load_metadata),
+    path('check-metadata/', views.check_metadata),
 ]
